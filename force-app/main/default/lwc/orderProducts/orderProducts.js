22465d66ab19beb9015ccfbe2eb53688b763d547
Bohdan Humeniuk <bhdn.humeniuk@gmail.com>
2023-07-16 17:41:54 +0300

    Sortable and remove are working

--- force-app/main/default/lwc/orderProducts/orderProducts.js
+++ force-app/main/default/lwc/orderProducts/orderProducts.js
@@ -2,36 +2,46 @@ import { LightningElement, wire, api, track } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import getOrderProducts from '@salesforce/apex/OrderProductsRepository.getOrderProducts';
 import activateOrder from '@salesforce/apex/OrderProductsRepository.activateOrder';
+import deleteProductFromOrder from '@salesforce/apex/OrderProductsRepository.deleteProductFromOrder';
+
 import { showSuccessMessage, showErrorMessage } from "c/showMessageHelper";
 
 const columns = [
     { label: 'Name', fieldName: 'productName', type: 'text', sortable: true },
-    { label: 'Quantity', fieldName: 'quantity', type: 'number', sortable: true },
     { label: 'Unit Price', fieldName: 'unitPrice', type: 'currency', sortable: true },
+    { label: 'Quantity', fieldName: 'quantityValue', type: 'number', sortable: true },
     { label: 'Total Price', fieldName: 'totalPrice', type: 'currency', sortable: true },
     { label: 'Action', type: 'button', typeAttributes: { label: 'Remove', name: 'remove', disabled: { fieldName: 'disableRemove' } } }
 ];
 
 export default class OrderProducts extends LightningElement {
-    @api recordId;
-    @track orderProducts = [];
+    
+    orderProducts = [];
     columns = columns;
     isOrderActivated = false;
+    sortDirection = 'asc';
+    sortedBy;
+    wiredOrderProductsResult;
+    activatedOrder;
+
+    @api recordId;
 
     @wire(getOrderProducts, { orderId: '$recordId' })
-    wiredOrderProducts({ data, error }) {
-        if (data) {
-            this.orderProducts = data.map(product => {
+    wiredOrderProducts(result) {
+        this.wiredOrderProductsResult = result;
+        if (result.data) {
+            this.orderProducts = result.data.map(product => {
                 return {
                     ...product,
                     productName: product.Product2.Name,
                     unitPrice: product.UnitPrice,
+                    quantityValue: product.Quantity,
                     totalPrice: product.UnitPrice * product.Quantity,
                     disableRemove: this.isOrderActivated
                 };
             });
-        } else if (error) {
-            console.error('Error fetching order products:', error);
+        } else if (result.error) {
+            console.error('Error fetching order products:', result.error);
         }
     }
 
@@ -49,10 +59,10 @@ export default class OrderProducts extends LightningElement {
             showErrorMessage('Error', 'Order is already activated. Cannot remove products.');
             return;
         }
-
-        deleteProductFromOrder({ orderId: this.recordId, orderItemId })
+    
+        deleteProductFromOrder({ orderItemId })
             .then(() => {
-                return refreshApex(this.wiredOrderProducts);
+                return refreshApex(this.wiredOrderProductsResult);
             })
             .then(() => {
                 showSuccessMessage('Success', 'Product removed from order successfully');
@@ -66,7 +76,11 @@ export default class OrderProducts extends LightningElement {
     handleActivateOrder() {
         activateOrder({ orderId: this.recordId })
             .then(() => {
+                return getOrderProducts({ orderId: this.recordId });
+            })
+            .then((data) => {
                 this.isOrderActivated = true;
+                this.activatedOrder = data[0]; // Assuming the response contains only one order record
                 this.updateRemoveButtons();
                 showSuccessMessage('Success', 'Order and products activated successfully');
             })
@@ -80,5 +94,28 @@ export default class OrderProducts extends LightningElement {
         this.orderProducts = this.orderProducts.map(product => {
             return { ...product, disableRemove: true };
         });
+    }
+
+    handleSort(event) {
+        const { fieldName: sortField, sortDirection } = event.detail;
+        this.sortedBy = sortField;
+        this.sortDirection = sortDirection === 'asc' ? 'asc' : 'desc';
+        this.sortData(sortField, this.sortDirection);
+    }
+
+    sortData(sortField, sortDirection) {
+        const data = JSON.parse(JSON.stringify(this.orderProducts));
+        data.sort((a, b) => {
+            let sortValue = 0;
+            const valueA = a[sortField] || '';
+            const valueB = b[sortField] || '';
+            if (sortDirection === 'asc') {
+                sortValue = valueA > valueB ? 1 : -1;
+            } else if (sortDirection === 'desc') {
+                sortValue = valueA < valueB ? 1 : -1;
+            }
+            return sortValue;
+        });
+        this.orderProducts = data;
     }
 }

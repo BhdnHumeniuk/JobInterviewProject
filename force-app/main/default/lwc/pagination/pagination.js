eb1278015780c0ec9829c5bb42fdee76dc8e0d41
BhdnHumeniuk <94472436+BhdnHumeniuk@users.noreply.github.com>
2023-08-16 13:38:52 +0300

    Update Pagination and create PaginationHelper

--- force-app/main/default/lwc/pagination/pagination.js
+++ force-app/main/default/lwc/pagination/pagination.js
@@ -1,40 +1,10 @@
-/**
- * @description This utility file contains the implementation of a pagination component to display a limited number of records
- *              per page and navigate between pages. It also provides event-based communication to update the visible records
- *              when the pagination changes.
- *
- * @api public
- *
- * @property {number} currentPage - The current active page number.
- * @property {number} totalRecords - The total number of records to paginate.
- * @property {number} recordSize - The number of records to display per page. Default is 5.
- * @property {number} totalPage - The total number of pages based on the totalRecords and recordSize.
- * @property {Array} visibleRecords - The subset of records displayed on the current active page.
- * @property {Array} recordSizeOptions - Options to select the number of records to display per page.
- *
- * @method get records() - Getter method to retrieve the visibleRecords property.
- * @method set records(data) - Setter method to set the totalRecords and update the pagination when data is provided.
- * @method get disablePrevious() - Getter method to check if the "Previous" button should be disabled.
- * @method get disableNext() - Getter method to check if the "Next" button should be disabled.
- * @method previousHandler() - Method to handle the "Previous" button click and update to the previous page.
- * @method nextHandler() - Method to handle the "Next" button click and update to the next page.
- * @method updateRecords() - Method to update the visibleRecords based on the currentPage and recordSize.
- * @method handleItemsPerPageChange(event) - Method to handle the change of items per page and update pagination accordingly.
- */
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 
 export default class Pagination extends LightningElement {
     currentPage = 1;
     totalRecords;
-    @api recordSize = 5;
+    _recordSize;
     totalPage = 0;
-    visibleRecords = [];
-    recordSizeOptions = [
-        { label: '5', value: '5' },
-        { label: '10', value: '10' },
-        { label: '20', value: '20' },
-        { label: '50', value: '50' }
-    ];
 
     get records() {
         return this.visibleRecords;
@@ -48,6 +18,16 @@ export default class Pagination extends LightningElement {
             this.totalPage = Math.ceil(data.length / this.recordSize);
             this.updateRecords();
         }
+    }
+
+    @api
+    get recordSize() {
+        return this._recordSize;
+    }
+
+    set recordSize(value) {
+        this._recordSize = value;
+        this.updateTotalPage();
     }
 
     get disablePrevious() {
@@ -85,10 +65,9 @@ export default class Pagination extends LightningElement {
         );
     }
 
-    handleItemsPerPageChange(event) {
-        this.recordSize = event.detail.value;
+    updateTotalPage() {
         this.totalPage = Math.ceil(this.totalRecords.length / this.recordSize);
-        this.currentPage = 1;
+        console.log(this.totalPage);
         this.updateRecords();
     }
 }
